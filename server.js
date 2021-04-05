970b20d4fac31cd09d7c02917c1903ccb6779eb4
chandra655 <chandra.penugonda@unacademy.com>
2021-04-05 10:04:35 +0530

    serve static folder

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const router = express.Router();
 const router1 = express.Router();
 
 const data = [{ id: 1, name: "test1" }];
+
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 router.get("/employees", (req, res) => res.send("Hello world"));
 router1.get("/employees", (req, res) => res.send(data));
